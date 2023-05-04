25d33b34f726c6bed55fe519fa89e7af06790359
Kamran Ali <Kamran Ali@KAMRAN-ALI>
2023-05-04 06:11:23 +0500

    Authentication

--- middleware/index.js
+++ middleware/index.js
@@ -1,23 +1,23 @@
 import admin from "firebase-admin";
 import User from "../models/user"
 import { expressjwt } from "express-jwt";
-var serviceAccount=require("../firebaseAdmin/ServiceKey.json");
+// var serviceAccount=require("../firebaseAdmin/ServiceKey.json");
 
-admin.initializeApp({
-    credential:admin.credential.cert(serviceAccount)
-})
-export const GoogleAuthCheck=async(req,res,next)=>{
-    try{
-    const firebaseuser=await admin.auth().verifyIdToken(req.headers.authcheck);
-     req.user=firebaseuser;
-     next();
-    }catch(error){
-    console.log("Google Auth Check Error");
-    return res.status(400).json({
-        error:"Invalid or Expired"
-    });
-    }
-    }
+// admin.initializeApp({
+//     credential:admin.credential.cert(serviceAccount)
+// })
+// export const GoogleAuthCheck=async(req,res,next)=>{
+//     try{
+//     const firebaseuser=await admin.auth().verifyIdToken(req.headers.authcheck);
+//      req.user=firebaseuser;
+//      next();
+//     }catch(error){
+//     console.log("Google Auth Check Error");
+//     return res.status(400).json({
+//         error:"Invalid or Expired"
+//     });
+//     }
+//     }
 
 export const requireSigin=expressjwt({
         secret: process.env.JWT_SECRET,
