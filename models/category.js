1a90f8c3c05e5f330b8c08fbd7d4b213c5de97c7
Farisbit19003 <farisirfanbit19.03@gmail.com>
2023-05-16 15:55:26 +0500

    PARENT CATEGORY REMOVED

--- models/category.js
+++ models/category.js
@@ -1,24 +1,23 @@
-const mongoose=require("mongoose");
-const {Schema}=mongoose;
-const CategorySchema =new Schema({
-    name:{
-        type:String,
-        trim:true,
-        required:true,
-        minlength:[2,"Too Short"],
-        maxlength:[32,"Too Long"]
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+const CategorySchema = new Schema(
+  {
+    name: {
+      type: String,
+      trim: true,
+      required: true,
+      minlength: [2, "Too Short"],
+      maxlength: [32, "Too Long"],
     },
-    slug:{
-        type:String,
-        unique:true,
-        lowercase:true,
+    slug: {
+      type: String,
+      unique: true,
+      lowercase: true,
     },
-    details:{
-        type:String,
+    details: {
+      type: String,
     },
-    ParentCategory:{
-        type:String,
-    },
-
-},{timestamps:true});
-export default mongoose.model("Category",CategorySchema);
+  },
+  { timestamps: true }
+);
+export default mongoose.model("Category", CategorySchema);
